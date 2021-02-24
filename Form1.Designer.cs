6acc3856a06ee08dd65cf0c2486c89b134ef2e50
Aliaksei Kakhavets <aliaksei_kakhavets@epam.com>
2021-02-24 23:42:10 +0300

    Lab1

---
+++ Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿
+namespace Lab1KG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hsvPicture = new System.Windows.Forms.PictureBox();
+            this.xyzPicture = new System.Windows.Forms.PictureBox();
+            this.labPicture = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.hField = new System.Windows.Forms.TextBox();
+            this.sField = new System.Windows.Forms.TextBox();
+            this.vField = new System.Windows.Forms.TextBox();
+            this.hsvButton = new System.Windows.Forms.Button();
+            this.xyzButton = new System.Windows.Forms.Button();
+            this.xFieldXYZ = new System.Windows.Forms.TextBox();
+            this.yFieldXYZ = new System.Windows.Forms.TextBox();
+            this.zFieldXYZ = new System.Windows.Forms.TextBox();
+            this.lField = new System.Windows.Forms.TextBox();
+            this.aField = new System.Windows.Forms.TextBox();
+            this.bField = new System.Windows.Forms.TextBox();
+            this.ShowLAB = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.hsvPicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xyzPicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.labPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // hsvPicture
+            // 
+            this.hsvPicture.Location = new System.Drawing.Point(31, 39);
+            this.hsvPicture.Name = "hsvPicture";
+            this.hsvPicture.Size = new System.Drawing.Size(260, 34);
+            this.hsvPicture.TabIndex = 1;
+            this.hsvPicture.TabStop = false;
+            // 
+            // xyzPicture
+            // 
+            this.xyzPicture.Location = new System.Drawing.Point(290, 39);
+            this.xyzPicture.Name = "xyzPicture";
+            this.xyzPicture.Size = new System.Drawing.Size(227, 34);
+            this.xyzPicture.TabIndex = 2;
+            this.xyzPicture.TabStop = false;
+            // 
+            // labPicture
+            // 
+            this.labPicture.Location = new System.Drawing.Point(514, 39);
+            this.labPicture.Name = "labPicture";
+            this.labPicture.Size = new System.Drawing.Size(229, 34);
+            this.labPicture.TabIndex = 3;
+            this.labPicture.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "HSV";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(287, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "XYZ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(536, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "LAB";
+            // 
+            // hField
+            // 
+            this.hField.Location = new System.Drawing.Point(66, 107);
+            this.hField.Name = "hField";
+            this.hField.Size = new System.Drawing.Size(50, 22);
+            this.hField.TabIndex = 7;
+            // 
+            // sField
+            // 
+            this.sField.Location = new System.Drawing.Point(122, 107);
+            this.sField.Name = "sField";
+            this.sField.Size = new System.Drawing.Size(50, 22);
+            this.sField.TabIndex = 8;
+            // 
+            // vField
+            // 
+            this.vField.Location = new System.Drawing.Point(178, 107);
+            this.vField.Name = "vField";
+            this.vField.Size = new System.Drawing.Size(50, 22);
+            this.vField.TabIndex = 9;
+            // 
+            // hsvButton
+            // 
+            this.hsvButton.Location = new System.Drawing.Point(57, 162);
+            this.hsvButton.Name = "hsvButton";
+            this.hsvButton.Size = new System.Drawing.Size(204, 23);
+            this.hsvButton.TabIndex = 10;
+            this.hsvButton.Text = "ShowHSV";
+            this.hsvButton.UseVisualStyleBackColor = true;
+            this.hsvButton.Click += new System.EventHandler(this.hsvButton_Click);
+            // 
+            // xyzButton
+            // 
+            this.xyzButton.Location = new System.Drawing.Point(290, 162);
+            this.xyzButton.Name = "xyzButton";
+            this.xyzButton.Size = new System.Drawing.Size(204, 23);
+            this.xyzButton.TabIndex = 11;
+            this.xyzButton.Text = "ShowXYZ";
+            this.xyzButton.UseVisualStyleBackColor = true;
+            this.xyzButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // xFieldXYZ
+            // 
+            this.xFieldXYZ.Location = new System.Drawing.Point(303, 107);
+            this.xFieldXYZ.Name = "xFieldXYZ";
+            this.xFieldXYZ.Size = new System.Drawing.Size(50, 22);
+            this.xFieldXYZ.TabIndex = 12;
+            // 
+            // yFieldXYZ
+            // 
+            this.yFieldXYZ.Location = new System.Drawing.Point(359, 107);
+            this.yFieldXYZ.Name = "yFieldXYZ";
+            this.yFieldXYZ.Size = new System.Drawing.Size(50, 22);
+            this.yFieldXYZ.TabIndex = 13;
+            // 
+            // zFieldXYZ
+            // 
+            this.zFieldXYZ.Location = new System.Drawing.Point(415, 107);
+            this.zFieldXYZ.Name = "zFieldXYZ";
+            this.zFieldXYZ.Size = new System.Drawing.Size(50, 22);
+            this.zFieldXYZ.TabIndex = 14;
+            // 
+            // lField
+            // 
+            this.lField.Location = new System.Drawing.Point(539, 107);
+            this.lField.Name = "lField";
+            this.lField.Size = new System.Drawing.Size(50, 22);
+            this.lField.TabIndex = 15;
+            // 
+            // aField
+            // 
+            this.aField.Location = new System.Drawing.Point(595, 107);
+            this.aField.Name = "aField";
+            this.aField.Size = new System.Drawing.Size(50, 22);
+            this.aField.TabIndex = 16;
+            // 
+            // bField
+            // 
+            this.bField.Location = new System.Drawing.Point(651, 107);
+            this.bField.Name = "bField";
+            this.bField.Size = new System.Drawing.Size(50, 22);
+            this.bField.TabIndex = 17;
+            // 
+            // ShowLAB
+            // 
+            this.ShowLAB.Location = new System.Drawing.Point(525, 162);
+            this.ShowLAB.Name = "ShowLAB";
+            this.ShowLAB.Size = new System.Drawing.Size(204, 23);
+            this.ShowLAB.TabIndex = 18;
+            this.ShowLAB.Text = "ShowLAB";
+            this.ShowLAB.UseVisualStyleBackColor = true;
+            this.ShowLAB.Click += new System.EventHandler(this.ShowLAB_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(786, 233);
+            this.Controls.Add(this.ShowLAB);
+            this.Controls.Add(this.bField);
+            this.Controls.Add(this.aField);
+            this.Controls.Add(this.lField);
+            this.Controls.Add(this.zFieldXYZ);
+            this.Controls.Add(this.yFieldXYZ);
+            this.Controls.Add(this.xFieldXYZ);
+            this.Controls.Add(this.xyzButton);
+            this.Controls.Add(this.hsvButton);
+            this.Controls.Add(this.vField);
+            this.Controls.Add(this.sField);
+            this.Controls.Add(this.hField);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labPicture);
+            this.Controls.Add(this.xyzPicture);
+            this.Controls.Add(this.hsvPicture);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.hsvPicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xyzPicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.labPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox hsvPicture;
+        private System.Windows.Forms.PictureBox xyzPicture;
+        private System.Windows.Forms.PictureBox labPicture;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox hField;
+        private System.Windows.Forms.TextBox sField;
+        private System.Windows.Forms.TextBox vField;
+        private System.Windows.Forms.Button hsvButton;
+        private System.Windows.Forms.Button xyzButton;
+        private System.Windows.Forms.TextBox xFieldXYZ;
+        private System.Windows.Forms.TextBox yFieldXYZ;
+        private System.Windows.Forms.TextBox zFieldXYZ;
+        private System.Windows.Forms.TextBox lField;
+        private System.Windows.Forms.TextBox aField;
+        private System.Windows.Forms.TextBox bField;
+        private System.Windows.Forms.Button ShowLAB;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,347 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1KG
+{
+    struct HSV
+    {
+        public int H;
+        public int S;
+        public int V;
+    }
+    struct RGB
+    {
+        public int R;
+        public int G;
+        public int B;
+    }
+    struct LAB
+    {
+        public double L;
+        public double A;
+        public double B;
+    }
+    struct XYZ
+    {
+        public double X;
+        public double Y;
+        public double Z;
+    }
+
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+
+
+        private void hsvButton_Click(object sender, EventArgs e)
+        {
+            HSV p;
+            p.H = Convert.ToInt32(hField.Text);
+            p.S = Convert.ToInt32(sField.Text);
+            p.V = Convert.ToInt32(vField.Text);
+            showColorsHSV(fromHSV(p));
+        }
+
+        private RGB fromHSV(HSV p)
+        {
+            int h = p.H;
+            int s = p.S;
+            int v = p.V;
+
+            if (h < 360 && h >= 0 && s <= 100 && s >= 0 && v >= 0 && v <= 100)
+            {
+                double C = v * s / 10000.0;
+                double X = C * (1 - Math.Abs((h / 60) % 2 - 1));
+                double m = v / 100.0 - C;
+
+                double rt, gt, bt;
+                if (h < 60 && h >= 0)
+                {
+                    rt = C;
+                    gt = X;
+                    bt = 0;
+                }
+                else if (h < 120 && h >= 60)
+                {
+                    rt = X;
+                    gt = C;
+                    bt = 0;
+                }
+                else if (h < 180 && h >= 120)
+                {
+                    rt = 0;
+                    gt = C;
+                    bt = X;
+                }
+                else if (h < 240 && h >= 180)
+                {
+                    rt = 0;
+                    gt = X;
+                    bt = C;
+                }
+                else if (h < 300 && h >= 240)
+                {
+                    rt = X;
+                    gt = 0;
+                    bt = C;
+                }
+                else
+                {
+                    rt = C;
+                    gt = 0;
+                    bt = X;
+                }
+                RGB p2;
+                p2.R = Math.Abs((int)((rt + m) * 255));
+                p2.G = Math.Abs((int)((gt + m) * 255));
+                p2.B = Math.Abs((int)((bt + m) * 255));
+                return p2;
+            }
+            RGB n;
+            n.R = 0;
+            n.G = 0;
+            n.B = 0;
+            return n;
+        }
+        private HSV toHSV(RGB r)
+        {
+            double rt = r.R / 255.0;
+            double gt = r.G / 255.0;
+            double bt = r.B / 255.0;
+
+            double cmax = Math.Max(rt, Math.Max(gt, bt));
+            double cmin = Math.Min(rt, Math.Min(gt, bt));
+            double delta = cmax - cmin;
+
+            HSV point;
+
+            if(delta == 0)
+            {
+                point.H = 0;
+            }
+            else if (cmax == rt)
+            {
+                point.H = (int)(60 * (((gt - bt) / delta) % 6));
+            }
+            else if (cmax == gt)
+            {
+                point.H = (int)(60 * (((bt - rt) / delta) +2));
+            }
+            else
+            {
+                point.H = (int)(60 * (((rt - gt) / delta) + 4));
+            }
+            if (point.H < 0)
+            {
+                point.H += 360;
+            }
+
+            if (cmax == 0)
+            {
+                point.S = 0;
+            }
+            else
+            {
+                point.S = (int)(delta *100 / cmax);
+            }
+            point.V = (int)(cmax*100);
+
+            return point;
+        }
+
+        double f1(double x)
+        {
+            if ( x >= 0.04045)
+            {
+                return Math.Pow((x + 0.055) / 1.055, 2.40);
+            }
+            else
+            {
+                return x / 12.92;
+            }
+        }
+
+        private XYZ toXYZ(RGB l)
+        {
+            int R = l.R;
+            int G = l.G;
+            int B = l.B;
+
+            double Rn = f1(R / 255.0) * 100;
+            double Gn = f1(G / 255.0) * 100;
+            double Bn = f1(B / 255.0) * 100;
+
+            XYZ p;
+            p.X = 0.412453 * Rn + 0.357580 * Gn + 0.180423 * Bn;
+            p.Y = 0.212671 * Rn + 0.715160 * Gn + 0.072169 * Bn;
+            p.Z = 0.019334 * Rn + 0.119193 * Gn + 0.950227 * Bn;
+
+            return p;
+        }
+
+        double f2(double x)
+        {
+            if (x >= 0.0031308)
+            {
+                return 1.055 * Math.Pow(x, 1 / 2.4) - 0.055;
+            }
+            else
+            {
+                return 12.92 * x;
+            }
+        }
+        private RGB fromXYZ(XYZ l)
+        {
+            double Rn = 3.2406 * l.X / 100.0 - 1.5372 * l.Y / 100.0 - 0.4986 * l.Z / 100.0;
+            double Gn = -0.9689 * l.X / 100.0 + 1.8758 * l.Y / 100.0 + 0.0415 * l.Z / 100.0;
+            double Bn = 0.0557 * l.X / 100.0 - 0.2040 * l.Y / 100.0 + 1.0570 * l.Z / 100.0;
+
+            RGB r;
+            r.R = (int)(Math.Abs((f2(Rn) * 255)));
+            if (r.R > 255)
+            {
+                r.R = 0;
+            }
+
+            r.G = (int)(Math.Abs((f2(Gn) * 255)))%255;
+            r.B = (int)(Math.Abs((f2(Bn) * 255)))%255;
+
+            return r;
+        }
+
+        private void showColorsHSV(RGB r)
+        {
+            hsvPicture.BackColor = Color.FromArgb(r.R, r.G, r.B);
+            hsvPicture.ForeColor = Color.FromArgb(r.R, r.G, r.B);
+
+            label1.Text = "HSV: " + r.R + " " + r.G + " " + r.B;
+
+            XYZ x = toXYZ(r);
+            xFieldXYZ.Text = x.X.ToString();
+            yFieldXYZ.Text = x.Y.ToString();
+            zFieldXYZ.Text = x.Z.ToString();
+
+            LAB lab = toLab(x);
+            lField.Text = lab.L.ToString();
+            aField.Text = lab.A.ToString();
+            bField.Text = lab.B.ToString();
+
+        }
+        private void showColorsXYZ(RGB r)
+        {
+            xyzPicture.BackColor = Color.FromArgb(r.R, r.G, r.B);
+            xyzPicture.ForeColor = Color.FromArgb(r.R, r.G, r.B);
+
+            label2.Text = "XYZ: " + r.R + " " + r.G + " " + r.B;
+
+            HSV h = toHSV(r);
+            hField.Text = h.H.ToString();
+            sField.Text = h.S.ToString();
+            vField.Text = h.V.ToString();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            XYZ p;
+            p.X = Convert.ToDouble(xFieldXYZ.Text);
+            p.Y = Convert.ToDouble(yFieldXYZ.Text);
+            p.Z = Convert.ToDouble(zFieldXYZ.Text);
+
+            RGB rgb = fromXYZ(p);
+
+            showColorsXYZ(rgb);
+
+            LAB l = toLab(p);
+            lField.Text = l.L.ToString();
+            aField.Text = l.A.ToString();
+            bField.Text = l.B.ToString();
+
+        }
+
+        // Lab
+        private double f3(double x)
+        {
+            if (x >= 0.008856)
+            {
+                return Math.Pow(x, 1.0 / 3);
+            }
+            else
+            {
+                return 7.787 * x + 16 / 116.0;
+            }
+        }
+
+        private LAB toLab(XYZ p)
+        {
+            LAB l;
+            l.L = 116.0 * f3(p.Y / 100.0) - 16;
+            l.A = 500 * (f3(p.X / 95.047) - f3(p.Y / 100.0));
+            l.B = 200 * (f3(p.Y / 100.0) - f3(p.Z / 108.883));
+
+            return l;
+        }
+
+        private double f4(double x)
+        {
+            if (x * x * x >= 0.008856)
+            {
+                return x * x * x * 1.0;
+            }
+            else
+            {
+                return (x - (16.0 / 166.0)) / 7.787 * 1.0;
+            }
+        }
+
+        private XYZ fromLab(LAB l)
+        {
+            XYZ x;
+            x.Y = f4((l.L + 16.0) / 116.0) * 95.047;
+            x.X = f4(l.A / 500.0 + (l.L + 16.0) / 116.0) * 100;
+            x.Z = f4((l.L + 16.0) / 116.0 - l.B / 200.0) * 108.883;
+            return x;
+        }
+
+        private void ShowLAB_Click(object sender, EventArgs e)
+        {
+            LAB l;
+            l.L = Convert.ToDouble(lField.Text);
+            l.A = Convert.ToDouble(aField.Text);
+            l.B = Convert.ToDouble(bField.Text);
+
+            XYZ x = fromLab(l);
+            xFieldXYZ.Text = x.X.ToString();
+            yFieldXYZ.Text = x.Y.ToString();
+            zFieldXYZ.Text = x.Z.ToString();
+
+            RGB r = fromXYZ(x);
+
+            label3.Text = "LAB: " + r.R + " " + r.G + " " + r.B;
+            labPicture.BackColor = Color.FromArgb(r.R, r.G, r.B);
+            labPicture.ForeColor = Color.FromArgb(r.R, r.G, r.B);
+
+            HSV h = toHSV(r);
+            hField.Text = h.H.ToString();
+            sField.Text = h.S.ToString();
+            vField.Text = h.V.ToString();
+        }
+    }
+}
